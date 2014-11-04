857f40516f7daa82c44d89922ac1272bdd650478
Mecin <marusm4@gmail.com>
2014-11-04 20:59:13 +0100

    Added insert method for signup button into sqlite by Uri

--- app/src/main/java/pl/dmcs/mecin/notesmanager/SignUp.java
+++ app/src/main/java/pl/dmcs/mecin/notesmanager/SignUp.java
@@ -2,12 +2,18 @@ package pl.dmcs.mecin.notesmanager;
 
 
 
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -23,10 +29,47 @@ public class SignUp extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        // Sigup button
+        Button signUpButton = (Button) view.findViewById(R.id.register_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = (EditText)getView().findViewById(R.id.user_name_sign_up);
+                EditText passEditText = (EditText)getView().findViewById(R.id.user_passwd_sign_up);
+
+                String user = userEditText.getText().toString();
+                String pass = passEditText.getText().toString();
+
+                // Simply check
+                if(!user.equals("") && !pass.equals("")) {
+
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(Tables.Users.USERNAME, user);
+                    contentValues.put(Tables.Users.PASSWORD, pass); // TODO add encryption
+
+                    Uri addUserUri = getActivity().getContentResolver().insert(Tables.Users.CONTENT_URI, contentValues);
+
+                    Toast.makeText(getActivity().getApplicationContext(), "New record inserted!", Toast.LENGTH_SHORT).show();
+                    // To get my ContentProvider for sql calls
+                    //getActivity().getContentResolver().insert("USERS" ,contentValues)
+
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Not inserted!", Toast.LENGTH_SHORT).show();
+                    //switchFragment();
+                }
+            }
+        });
+
+
+
+        return view;
     }
 
 
